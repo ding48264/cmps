2b69fa7c6796a7ea022c5be1add753dafb1eee0f
Zourman <2941379493@qq.com>
2021-09-22 20:09:08 +0800

    登录界面和忘记密码界面

--- src/main/java/com/example/gllg/isp/cmps/controller/TbAdminController.java
+++ src/main/java/com/example/gllg/isp/cmps/controller/TbAdminController.java
@@ -1,12 +1,15 @@
 package com.example.gllg.isp.cmps.controller;
 
 import com.example.gllg.isp.cmps.entity.TbAdmin;
+import com.example.gllg.isp.cmps.entity.TbUptown;
 import com.example.gllg.isp.cmps.service.TbAdminService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * (TbAdmin)表控制层
@@ -33,5 +36,11 @@ public class TbAdminController {
     public TbAdmin selectOne(Integer id) {
         return this.tbAdminService.queryById(id);
     }
-
+    @RequestMapping
+    public ModelAndView Admin() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("Admin", Admin());
+        modelAndView.setViewName("uptown/Uptown");
+        return modelAndView;
+    }
 }
